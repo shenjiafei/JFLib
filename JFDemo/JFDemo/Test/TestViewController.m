60a8046f2c6598fcc8baaf187729820ca4dbb5f1
shenjiafei <pxdy001@gmail.com>
2021-02-23 16:00:36 +0800

    OC Swift 调用

---
+++ JFDemo/JFDemo/Test/TestViewController.m
@@ -0,0 +1,32 @@
+//
+//  TestViewController.m
+//  JFDemo
+//
+//  Created by shenjiafei on 2021/2/23.
+//  Copyright © 2021 shenjiafei. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.grayColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- JFDemo/JFDemo/ViewController.swift
+++ JFDemo/JFDemo/ViewController.swift
@@ -12,9 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.isHidden = false
     }
 
+    //extension 方式实现
 
+    lazy private var tableView:UITableView = {
+        let tableView = UITableView(frame:CGRect.init(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height - 100), style: UITableView.Style.plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .lightGray
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        self.view.addSubview(tableView)
+        return tableView
+    }()
 }
 
+extension
+ViewController:UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = "11"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = TestViewController()
+        let navVC = UINavigationController.init(rootViewController: vc)
+        present(navVC, animated: true, completion: nil)
+//        navigationController?.pushViewController(navVC, animated: true)
+    }
+    
+}
